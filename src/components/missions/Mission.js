99cedf36f526708d3af5a530a7f4aefcd8dc15f1
freddykaberuka <freddykaberuka2009@gmail.com>
2022-11-17 07:24:20 +0200

    design mission

---
+++ src/components/missions/Mission.js
@@ -0,0 +1,70 @@
+import React from 'react';
+// import MissionHeader from './MissionHeader';
+import './mission.css';
+
+function Mission() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th className="">Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Thaicom</td>
+            <td>
+              daofksdfsdfsdfsdlfksdlfdvnksdsdjfnsdkfklfskldfsdfsjvnjfvmdlfsfnjvcsfjskfsf
+              sdfjsdfsdfskncskdnfskldfklsdjfnlksf sdfknvjfknskmfs
+              sdfnsldflsdkfsdfslfnsdfsldnfv jisodfsdfsdklmcd
+              sdnfjfrsssdlfk skdnfjdscs skcnsjdnfglfssjnscslsm sdfknsjd
+              scsknncksnksdncksdvla.,mncsdn jksdcjksdc
+
+            </td>
+            <td><button type="button" className="member">NOT A MEMBER</button></td>
+            <td><button type="button" className="member-join"> Join Mission</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    // <div className="mission-container">
+    //   <MissionHeader />
+    //   <div className="mission-container-wrap">
+    //     <div className="mission-name"><h3>Thaicom</h3></div>
+    //     <div>
+    //       daofksdfsdfsdfsdlfksdlfdvnksdsdjfnsdkfklfskldfsdfsjvnjfvmdlfsfnjvcsfjskfsf
+    //       sdfjsdfsdfskncskdnfskldfklsdjfnlksf sdfknvjfknskmfs
+    //       sdfnsldflsdkfsdfslfnsdfsldnfv jisodfsdfsdklmcd
+    //       sdnfjfrsssdlfk skdnfjdscs skcnsjdnfglfssjnscslsm sdfknsjd
+    //       scsknncksnksdncksdvla.,mncsdn jksdcjksdc
+    //     </div>
+    //     <div>
+    //       <button type="button" className="member">NOT A MEMBER</button>
+
+  //     </div>
+  //     <div><button type="button" className="member-join"> Join Mission</button></div>
+  //   </div>
+  //   <div className="mission-container-wrap">
+  //     <div><h3>Thaicom</h3></div>
+  //     <div>
+  //       daofksdfsdfsdfsdlfksdlfdvnksdsdjfnsdkfklfskldfsdfsjvnjfvmdlfsfnjvcsfjskfsf
+  //       sdfjsdfsdfskncskdnfskldfklsdjfnlksf sdfknvjfknskmfs
+  //       sdfnsldflsdkfsdfslfnsdfsldnfv jisodfsdfsdklmcd
+  //       sdnfjfrsssdlfk skdnfjdscs skcnsjdnfglfssjnscslsm sdfknsjd
+  //       scsknncksnksdncksdvla.,mncsdn jksdcjksdc
+  //     </div>
+  //     <div>
+  //       <button type="button" className="a-member">Active Member</button>
+
+  //     </div>
+  //     <div><button type="button" className="member-leave"> Leave Mission</button></div>
+  //   </div>
+  // </div>
+  );
+}
+
+export default Mission;
