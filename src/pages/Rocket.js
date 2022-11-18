e4bfbc92208b0a4f75dcaec57c79eb97c872479e
Joel-100 <joelkugonza@gmail.com>
2022-11-18 11:21:45 +0300

    Add rocket component

--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllRockets } from '../redux/rocket/rockets';
+import { fetchAllRockets, toggleReservation } from '../redux/rocket/rockets';
 import './style.css';
 
 const Rocket = () => {
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
+  const reserveRocket = (id) => {
+    dispatch(toggleReservation(id));
+  };
+
   useEffect(() => {
     dispatch(fetchAllRockets());
   }, []);
 
-  console.log(rockets);
-
   return (
-    <div className="container">
+    <div className="rocket-container">
       {rockets.map((item) => (
         <div className="rocket-card" key={item.id}>
           <div className="rocket-profile">
@@ -22,8 +24,11 @@ const Rocket = () => {
           </div>
           <div className="rocket-description">
             <h1>{item.rockets_name}</h1>
-            <p>{item.description}</p>
-            <button type="button" className="btn">Reserve Rocket</button>
+            <p>
+              {item && item.reserved && <span className="reserved-badge">Reserved</span>}
+              {item.description}
+            </p>
+            {item.reserved ? <button type="button" className="cancel-btn" onClick={() => reserveRocket(item.id)}>Cancel Reservation</button> : <button type="button" className="btn" onClick={() => reserveRocket(item.id)}>Reserve Rocket</button>}
           </div>
         </div>
       ))}
